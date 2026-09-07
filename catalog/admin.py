8d85aa739ac3707099ee20e974cd138d84feb17d
user_l <zaripova_leysan@mail.ru>
2026-09-07 19:56:45 +0300

    бновлён .gitignore: добавлены .idea/ и .env

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Category, Product
 
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
