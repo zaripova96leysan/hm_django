fad71d959d067c0efb6e7c0bc3def2046dec29a0
user_l <zaripova_leysan@mail.ru>
2026-09-17 19:47:13 +0300

    homew

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
+    path("products/create/", views.product_create, name="product_create"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import ProductForm
 from .models import Product
 
-def product_list(request):
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
 
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            return redirect("product_detail", pk=product.pk)
+    else:
+        form = ProductForm()
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'product_detail.html', {'product':product})
+    return render(request, "product_form.html", {"form": form})
 
