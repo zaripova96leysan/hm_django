59a86c20acf8a912a7b43b43a45689100a39531f
user_l <zaripova_leysan@mail.ru>
2026-09-23 18:54:07 +0300

    Исправлены кавычки в urls.py

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
-    path("products/create/", views.ProductCreateView.as_view(), name="product_create"),
+    path('products/create/', views.ProductCreateView.as_view(), name='product_create'),
 ]
